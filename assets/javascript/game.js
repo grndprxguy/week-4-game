79d6668e14c89fb6076f6a86888ddd226feed238
grndprxguy <matthew.nagy@me.com>
2017-09-13 18:25:55 -0400

    working logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,17 @@
-$( document ).ready(function() {
 var wins = 0;
 var losses = 0;
 
+$(document).ready(function() {
+
+
 	function game(){
 	// variables
+	var total = 0;
 	// set game number between 19-120
 	var gameNum = random();
 	function random() {return Math.ceil(Math.random() * (120 - 19) + 19);};
 	// function to set crystal value 1-12
 	var rand = function() {return Math.ceil(Math.random() * (12 - 1) + 1);};
-	
-	var total = 0;
-
-	function gameover(){
-		if (total == gameNum) {
-		alert("Congrats, you win!");
-		}
-		else if (total > gameNum) {
-		alert("You Lose");
-		}
-	}
 	// set crystal values
 	var crystals = {
 		crystal1: rand(),
@@ -27,11 +19,26 @@ var losses = 0;
 		crystal3: rand(),
 		crystal4: rand(),
 		}
-		console.log(crystals);
 
+	// gameover logic
+	function gameover(){
+		if (total == gameNum) {
+		wins++;	
+		alert("Congrats, you win!");
+		$("#winCount").text(wins);
+		game();
+		}
+		else if (total > gameNum) {
+		losses++;
+		alert("You Lose");
+		$("losecount").text(losses);
+		game();
+		}
+	}
+	
 	// push gameNum to html
 	$("#random").text(gameNum);
-
+	
 	// push clicked crystal to crystalValue
 	$(".container").on('click',"div", function(){ 
 		var crystalClick = $(this).attr('id');
