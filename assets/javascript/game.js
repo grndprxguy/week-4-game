8d52bc10b45e1cb9913d6c22f5929ff446344c9d
grndprxguy <matthew.nagy@me.com>
2017-09-13 17:32:03 -0400

    working logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,43 @@
 $( document ).ready(function() {
-// variables
-// set game number between 19-120
-var gameNum = function() {return Math.ceil(Math.random() * (120 - 19) + 19);};
-// function to set crystal value 1-12
-var rand = function() {return Math.ceil(Math.random() * (12 - 1) + 1);};
 var wins = 0;
 var losses = 0;
-var total = 0;
-console.log(gameNum());
 
-// set crystal values
-var crystals = {
-	crystal1: rand(),
-	crystal2: rand(),
-	crystal3: rand(),
-	crystal4: rand(),
+	function game(){
+	// variables
+	// set game number between 19-120
+	var gameNum = random();
+	function random() {return Math.ceil(Math.random() * (120 - 19) + 19);};
+	// function to set crystal value 1-12
+	var rand = function() {return Math.ceil(Math.random() * (12 - 1) + 1);};
+	
+	var total = 0;
+
+	function gameover(){
+		if (total == gameNum) {
+		alert("Congrats, you win!");
+		}
+		else if (total > gameNum) {
+		alert("You Lose");
+		}
 	}
-	console.log(crystals);
+	// set crystal values
+	var crystals = {
+		crystal1: rand(),
+		crystal2: rand(),
+		crystal3: rand(),
+		crystal4: rand(),
+		}
+		console.log(crystals);
 
-// push gameNum to html
-$("#random").text(gameNum);
+	// push gameNum to html
+	$("#random").text(gameNum);
 
-// push clicked crystal to crystalValue
-$(".container").on('click', function(){ 
-	$("#totalScore").text(total += crystals.crystal1)
-});	
-
-
+	// push clicked crystal to crystalValue
+	$(".container").on('click',"div", function(){ 
+		var crystalClick = $(this).attr('id');
+		$("#totalScore").text(total += crystals[crystalClick]);
+		gameover();
+	});	
+};
+game();
 });
